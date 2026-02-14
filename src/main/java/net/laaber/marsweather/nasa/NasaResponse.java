32368b3a4c4e279a2622007ec942c8b10ca61ea0
Christoph Laaber <christoph.laaber@gmail.com>
2026-02-14 08:58:00 +0100

    implemented proper NasaClientTest

--- src/main/java/net/laaber/marsweather/nasa/NasaResponse.java
+++ src/main/java/net/laaber/marsweather/nasa/NasaResponse.java
@@ -1,6 +1,9 @@
 package net.laaber.marsweather.nasa;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.Map;
 
-public record NasaResponse(Map<String, String> descriptions, List<SolInfo> soles) {}
+public record NasaResponse(
+        @JsonProperty("descriptions") Map<String, String> descriptions,
+        @JsonProperty("soles") List<SolInfo> soles) {}
--- src/test/java/net/laaber/marsweather/nasa/NasaClientTest.java
+++ src/test/java/net/laaber/marsweather/nasa/NasaClientTest.java
@@ -11,6 +11,9 @@ import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -23,6 +26,8 @@ import org.springframework.test.context.DynamicPropertySource;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NasaClientTest {
+
+    private static int SOLES_COUNT = 4551;
 
     private WireMockServer wireMockServer;
 
@@ -63,11 +68,78 @@ public class NasaClientTest {
         var response = nasaClient.getMarsWeather();
         assertThat(response).isNotNull();
 
+        checkResponse(response);
+
         // Verify the call was made
         verify(getRequestedFor(urlPathEqualTo("/"))
                 .withQueryParam("feed", equalTo("weather"))
                 .withQueryParam("feedtype", equalTo("json"))
                 .withQueryParam("ver", equalTo("1.0"))
                 .withQueryParam("category", equalTo("msl")));
+    }
+
+    private void checkResponse(NasaResponse response) {
+        assertThat(response.descriptions()).isNotNull();
+
+        // check soles
+        assertThat(response.soles()).describedAs("soles is null").isNotNull();
+
+        var soles = response.soles();
+        assertThat(soles.size()).describedAs("soles.size() incorrect").isEqualTo(SOLES_COUNT);
+
+        var solesMap = soles.stream().collect(Collectors.toMap(SolInfo::sol, Function.identity()));
+        assertThat(solesMap.size()).describedAs("solesMap.size() incorrect").isEqualTo(SOLES_COUNT);
+
+        // check individual soles
+        checkSol1(solesMap);
+        checkSol4801(solesMap);
+    }
+
+    private void checkSol1(Map<String, SolInfo> soles) {
+        var sol = "1";
+        var solInfo = soles.get(sol);
+        assertThat(solInfo).isNotNull();
+        assertThat(solInfo.id()).isEqualTo("1");
+        assertThat(solInfo.terrestrialDate()).isEqualTo("2012-08-07");
+        assertThat(solInfo.sol()).isEqualTo(sol);
+        assertThat(solInfo.solarLongitude()).isEqualTo("150");
+        assertThat(solInfo.season()).isEqualTo("Month 6");
+        assertThat(solInfo.minAirTemperature()).isEqualTo("--");
+        assertThat(solInfo.maxAirTemperature()).isEqualTo("--");
+        assertThat(solInfo.atmosphericPressure()).isEqualTo("--");
+        assertThat(solInfo.pressureString()).isEqualTo("Lower");
+        assertThat(solInfo.absoluteHumidity()).isEqualTo("--");
+        assertThat(solInfo.windSpeed()).isEqualTo("--");
+        assertThat(solInfo.windDirection()).isEqualTo("--");
+        assertThat(solInfo.atmosphericOpacity()).isEqualTo("Sunny");
+        assertThat(solInfo.sunrise()).isEqualTo("05:30");
+        assertThat(solInfo.sunset()).isEqualTo("17:22");
+        assertThat(solInfo.localUvIrradianceIndex()).isEqualTo("--");
+        assertThat(solInfo.minGroundTemperature()).isEqualTo("--");
+        assertThat(solInfo.maxGroundTemperature()).isEqualTo("--");
+    }
+
+    private void checkSol4801(Map<String, SolInfo> soles) {
+        var sol = "4801";
+        var solInfo = soles.get(sol);
+        assertThat(solInfo).isNotNull();
+        assertThat(solInfo.id()).isEqualTo("4551");
+        assertThat(solInfo.terrestrialDate()).isEqualTo("2026-02-07");
+        assertThat(solInfo.sol()).isEqualTo(sol);
+        assertThat(solInfo.solarLongitude()).isEqualTo("221");
+        assertThat(solInfo.season()).isEqualTo("Month 8");
+        assertThat(solInfo.minAirTemperature()).isEqualTo("-67");
+        assertThat(solInfo.maxAirTemperature()).isEqualTo("2");
+        assertThat(solInfo.atmosphericPressure()).isEqualTo("807");
+        assertThat(solInfo.pressureString()).isEqualTo("Higher");
+        assertThat(solInfo.absoluteHumidity()).isEqualTo("--");
+        assertThat(solInfo.windSpeed()).isEqualTo("--");
+        assertThat(solInfo.windDirection()).isEqualTo("--");
+        assertThat(solInfo.atmosphericOpacity()).isEqualTo("Sunny");
+        assertThat(solInfo.sunrise()).isEqualTo("05:23");
+        assertThat(solInfo.sunset()).isEqualTo("17:34");
+        assertThat(solInfo.localUvIrradianceIndex()).isEqualTo("Moderate");
+        assertThat(solInfo.minGroundTemperature()).isEqualTo("-83");
+        assertThat(solInfo.maxGroundTemperature()).isEqualTo("12");
     }
 }
